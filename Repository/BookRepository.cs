564f4bb7570b11132878d70dc9518bf71bdc349d
Błażej Wróbel <blazej.wrobel@microsoft.wsei.edu.pl>
2022-02-27 12:21:55 +0100

    Task 3

--- Repository/BookRepository.cs
+++ Repository/BookRepository.cs
@@ -2,10 +2,8 @@ using LibApp.Data;
 using LibApp.Interfaces;
 using LibApp.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace LibApp.Repository
 {
